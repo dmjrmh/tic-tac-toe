d3ce6f57ccdebe7a50b5a10a447e4d4fc3df5c48
Dame <gban1mj27@gmail.com>
2025-05-06 17:43:46 +0700

    first commit tic tac toe

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useState } from 'react'
+
+function Square({value, onSquareClick}) {
+  // const [value, setValue] = useState('');
+
+  // function handleClick(){
+  //   setValue('X')
+  // }
+
+  return <button className="square" onClick={onSquareClick}>{value}</button>
+}
+
+export default function Board() { 
+
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  function handleClick(i) {
+    const nextSquares = squares.slice()
+    // console.log(nextSquares);
+
+    // if (xIsNext) {
+    //   nextSquares[i] = 'X'
+    // } else {
+    //   nextSquares[i] = 'O'
+    // }
+
+    if (squares[i] || calculateWinner(squares)) {
+      return; 
+    }
+
+    nextSquares[i] = xIsNext ? 'X' : 'O'
+    setSquares(nextSquares)
+    setXIsNext(!xIsNext)
+  }
+
+  const winner = calculateWinner(squares)
+  // console.log(winner);
+  let status = ''
+  if (winner) {
+    status = 'Winner : ' + winner 
+  } else {
+    status = 'Next player : ' + (xIsNext ? 'X' : 'O')
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </>
+  )
+}
+
+function calculateWinner(squares){
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ] 
+
+  for(let i=0; i < lines.length; i ++){
+    // const a = lines[i][0] // 0
+    // const b = lines[i][1] // 1
+    // const c = lines[i][2] // 2
+
+    // destructuring
+
+    const[a, b, c] = lines[i]
+
+    if (squares[a] && squares[a] === squares[b] && squares[c]){
+      return squares[a]
+    }
+  }
+  return false
+}
