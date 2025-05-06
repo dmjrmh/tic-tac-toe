cdd4e484ca1f112f45bbd872509b7cb6c72762ff
Dame <gban1mj27@gmail.com>
2025-05-06 18:13:52 +0700

    added time travel features for rollback moves

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,18 @@ function Square({value, onSquareClick}) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
 }
 
-export default function Board() { 
-
-  const [squares, setSquares] = useState(Array(9).fill(null))
-  const [xIsNext, setXIsNext] = useState(true)
+function Board({xIsNext, squares, onPlay}) { 
 
   function handleClick(i) {
     const nextSquares = squares.slice()
-    // console.log(nextSquares);
-
-    // if (xIsNext) {
-    //   nextSquares[i] = 'X'
-    // } else {
-    //   nextSquares[i] = 'O'
-    // }
 
     if (squares[i] || calculateWinner(squares)) {
       return; 
     }
 
     nextSquares[i] = xIsNext ? 'X' : 'O'
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+
+    onPlay(nextSquares)
   }
 
   const winner = calculateWinner(squares)
@@ -61,6 +51,55 @@ export default function Board() {
   )
 }
 
+export default function Game() {
+  // const [xIsNext, setXIsNext] = useState(true)
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0)
+  const xIsNext = currentMove % 2 === 0
+  const currentSquares = history[currentMove]
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove)
+    // setXIsNext(nextMove % 2 === 0)
+  }
+
+  
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1 )
+    // setXIsNext(!xIsNext)
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = ''
+    if(move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start'
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  return (
+    <>
+      <div className="game">
+        <div className="game-board">
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+        </div>
+        <div className="game-info">
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    </>
+  )
+}
+
 function calculateWinner(squares){
   const lines = [
     [0,1,2],
@@ -73,16 +112,16 @@ function calculateWinner(squares){
     [2,4,6]
   ] 
 
-  for(let i=0; i < lines.length; i ++){
+  for (let i = 0; i < lines.length; i++) {
     // const a = lines[i][0] // 0
     // const b = lines[i][1] // 1
     // const c = lines[i][2] // 2
 
     // destructuring
 
-    const[a, b, c] = lines[i]
+    const [a, b, c] = lines[i]
 
-    if (squares[a] && squares[a] === squares[b] && squares[c]){
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]
     }
   }
